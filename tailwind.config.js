fa29b5af664a8e3bc426ac65ba5dc73c1a0ccb53
hkgnp <ben@pngs.cc>
2021-11-11 09:31:57 +0800

    Troubleshooting delay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ['./views/**/*.{vue,js,ts,jsx,tsx}'],
+    content: ['./views/**/*.{vue,js,ts,jsx,tsx,hbs}'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
