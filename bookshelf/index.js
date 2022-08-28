0497f1e46720331cb2c558cc5b2b378a5d3ad61d
hkgnp <ben@pngs.cc>
2022-08-28 15:54:01 +0800

    -

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,15 +1,16 @@
-const knex = require('knex')({
+const knex = require("knex")({
   client: process.env.DB_DRIVER,
   connection: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl: false,
+    //ssl: {
+    //  rejectUnauthorized: false,
+    //},
   },
 });
-const bookshelf = require('bookshelf')(knex);
+const bookshelf = require("bookshelf")(knex);
 
 module.exports = bookshelf;
